3ab58c134b13653c269de98291f903f20cd602a9
Takahiro Tsuchiya <lecieldesylphi.d@gmail.com>
2012-04-25 12:45:06 +0900

    test3

---
+++ bps-html/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<!--[if IE 6]>
+<html id="ie6" dir="ltr" lang="ja">
+<![endif]-->
+<!--[if IE 7]>
+<html id="ie7" dir="ltr" lang="ja">
+<![endif]-->
+<!--[if IE 8]>
+<html id="ie8" dir="ltr" lang="ja">
+<![endif]-->
+<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
+<html dir="ltr" lang="ja">
+<!--<![endif]-->
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width" />
+		<title>ホーム | My First Homepage</title>
+		<link rel="profile" href="http://gmpg.org/xfn/11" />
+		
+		<? /* CSS(google font, jquery, user) */ ?>
+		<link href='http://fonts.googleapis.com/css?family=Poiret+One|Great+Vibes' rel='stylesheet' type='text/css' />
+		<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/blitzer/jquery-ui.css" rel="stylesheet" type="text/css"/>
+		<link rel ="stylesheet" href ="./style.css" type="text/css" />
+		
+		<? /* js */ ?>
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js"></script>
+		<script type="text/javascript"  src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.12/jquery-ui.min.js"></script>
+		
+	</head>
+	<body class="home page">
+		
+	</body>
+</html>
