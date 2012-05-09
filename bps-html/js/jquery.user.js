dd04bf20540569f509da205556de2f1ccd6b70ae
Takahiro Tsuchiya <lecieldesylphi.d@gmail.com>
2012-05-09 18:14:40 +0900

    All Done.
    images(PSD,GIF,PNG,JPG)+HTML5+CSS3+jQuery

--- bps-html/js/jquery.user.js
+++ bps-html/js/jquery.user.js
@@ -1,5 +1,78 @@
 (function($) {
 	$(document).ready(function(){
+		/* タブ関係 */
+			//init(詳細入力領域を閉じる)
+			$("#employFormInnerAdd").css("display","none");
+			//
+			$("#buttom_more").click(function () {
+				$("#employFormInnerAdd").slideToggle();
+			});
+		
+		/* 能力要件タブ関係 */
+			//初期状態にリセット
+			$(".tabcontent").css("display","none");
+			$("#tabcontent01").css("display","block");
+			
+			if (true) {
+				//タブにカーソルを合わせた時の動作
+				$("li#tab01").hover(
+					function () {
+						$(".menutabitem").css("background-position","left top");
+						$(this).css("background-position","left bottom");
+						$(".tabcontent").css("display","none");
+						$("#tabcontent01").css("display","block");
+					},
+					function () {
+					}
+				);
+				$("li#tab02").hover(
+					function () {
+						$(".menutabitem").css("background-position","left top");
+						$(this).css("background-position","left bottom");
+						$(".tabcontent").css("display","none");
+						$("#tabcontent02").css("display","block");
+					},
+					function () {
+					}
+				);
+				$("li#tab03").hover(
+					function () {
+						$(".menutabitem").css("background-position","left top");
+						$(this).css("background-position","left bottom");
+						$(".tabcontent").css("display","none");
+						$("#tabcontent03").css("display","block");
+					},
+					function () {
+					}
+				);
+				$("li#tab04").hover(
+					function () {
+						$(".menutabitem").css("background-position","left top");
+						$(this).css("background-position","left bottom");
+						$(".tabcontent").css("display","none");
+						$("#tabcontent04").css("display","block");
+					},
+					function () {
+					}
+				);
+				$("li#tab05").hover(
+					function () {
+						$(".menutabitem").css("background-position","left top");
+						$(this).css("background-position","left bottom");
+						$(".tabcontent").css("display","none");
+						$("#tabcontent05").css("display","block");
+					},
+					function () {
+					}
+				);
+			}
+		/* ページトップリンクの動作関係 */
+			$(function(){
+				$("a#toTop").click(function(){
+					$('html,body').animate({ scrollTop: $($(this).attr("href")).offset().top }, 'slow','swing');
+					return false;
+				})
+			});
 		
 	});
 })(jQuery);
